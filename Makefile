da95984429d7bde12b429065a808ecbb7177f07a
Simon Greuter <6537188+greuters@users.noreply.github.com>
2022-02-17 17:57:44 +0100

    Upgrade requirements and add coverage

--- Makefile
+++ Makefile
@@ -4,6 +4,12 @@ init:
 test:
 	python -m tests.scenario_medium
 
+coverage:
+	coverage run --branch -m tests.scenario_medium && coverage html
+
+upgrade:
+	pip install --upgrade pip && pip install --upgrade pip-tools && rm requirements.txt && pip-compile requirements.in
+
 docs:
 	$(MAKE) -C docs html
 
--- tests/context.py
+++ tests/context.py
@@ -8,5 +8,6 @@ import tagtrail.helpers as helpers
 import tagtrail.sheets as sheets
 import tagtrail.database as database
 import tagtrail.tagtrail_ocr as tagtrail_ocr
+import tagtrail.tagtrail_sanitize as tagtrail_sanitize
 import tagtrail.tagtrail_gen as tagtrail_gen
 import tagtrail.tagtrail_account as tagtrail_account
